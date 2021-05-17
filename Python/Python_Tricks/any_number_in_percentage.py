d71014de297cefd42dbeed73d87c85511227f322
mc4luna <77466718+mc4luna@users.noreply.github.com>
2021-05-17 07:16:03 -0500

    Create any_number_in_percentage.py
    #This funtion can return the percentage of any variable

---
+++ Python/Python_Tricks/any_number_in_percentage.py
@@ -0,0 +1,6 @@
+#This funtion can return the percentage of any variable
+def any_number_in_percentage(num1, num2):
+  #where num1 is the number that we calculate the % rate
+  sum_total = num1 + num2
+  percentage = int((num1/total)*100)
+  return percentage
