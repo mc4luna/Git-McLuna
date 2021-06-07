3452aa8c2fec0e2dd69e2a264ca61063fb758ebc
mc4luna <77466718+mc4luna@users.noreply.github.com>
2021-06-07 11:38:18 -0500

    Create python_strings_methods.py
    useful python methods

---
+++ Python/Python_Tricks/python_strings_methods.py
@@ -0,0 +1,8 @@
+print('Hello World'.upper())
+print('Hello World'.lower())
+print('Hello World'.title())
+print('Hello World'.split()) #Así hace el split por espacios
+print('Hello World'.split('o'))  # Así hace el split por letra
+print(' '.join(['Hello', 'World']))
+print('Hello Horld'.replace('H', 'j', 1)) #Letra a remplazar, letra a cambiar, posicion del string
+print('   Hello World   '.strip())
