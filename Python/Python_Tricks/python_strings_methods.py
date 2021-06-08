aa4eaf1308a492991423d00d842f465d7aca9e0a
mc4luna <77466718+mc4luna@users.noreply.github.com>
2021-06-08 08:36:30 -0500

    Update python_strings_methods.py
    More methods

--- Python/Python_Tricks/python_strings_methods.py
+++ Python/Python_Tricks/python_strings_methods.py
@@ -1,8 +1,27 @@
 print('Hello World'.upper())
 print('Hello World'.lower())
 print('Hello World'.title())
-print('Hello World'.split()) #Así hace el split por espacios
-print('Hello World'.split('o'))  # Así hace el split por letra
+
 print(' '.join(['Hello', 'World']))
 print('Hello Horld'.replace('H', 'j', 1)) #Letra a remplazar, letra a cambiar, posicion del string
 print('   Hello World   '.strip())
+
+#División de strings
+#1. Así se hace la division de un string por espacios
+print('Hello World'.split())
+#2. Así se hace la división por letra o caracter
+print('Hello World'.split('o'))
+#3. Así se hace la división por cada nueva línea
+print('''Hello World'''.split('\n'))
+#4. Así se hace la división por cada nueva tabulación
+print('''Hello World'''.split('\n'))
+
+#Unión de strings
+hello = ['Hello', 'word']
+hello_2 = ['Hola!','Mi nombre es manuela', 'Tengo 19 años' ]
+#1. Así se hace para unir 2 strings con espacios y de una variable
+print(' '.join(hello))
+#2. Así se hace para unir 2 strings definiendolo en la funcion
+print(' '.join('Hello','world'))
+#3. Así se hace para unir 2 strings por tabulacion y que quede en ese formato
+print('\n'.join(hello_2))
