fb7bd2db812b5934ff46a27f4ee9eba3b0f123a6
mc4luna <77466718+mc4luna@users.noreply.github.com>
2021-06-10 06:19:29 -0500

    Create module.py

---
+++ Python/Python_Tricks/module.py
@@ -0,0 +1,15 @@
+# MODULES
+# A module is a collection of Python declarations intended broadly to be used as a tool.
+# Modules are also often referred to as “libraries” or “packages” — 
+# (a package is really a directory that holds a collection of modules.)
+
+#Ex: from module_name import object_name
+# Or import object_name
+
+import random
+
+random_list = []
+
+
+#print()
+
