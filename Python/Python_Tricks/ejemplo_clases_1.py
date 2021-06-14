868f241d5b8e29118b252d687ff748347b5efdb4
mc4luna <77466718+mc4luna@users.noreply.github.com>
2021-06-14 06:45:38 -0500

    Update ejemplo_clases_1.py

--- Python/Python_Tricks/ejemplo_clases_1.py
+++ Python/Python_Tricks/ejemplo_clases_1.py
@@ -1,7 +1,7 @@
 #This is a way to import another funtion of a file in python
 
 # Import library below:
-from library import always_three
+from ejemplo_de_clases_2 import always_three
 
 
 # Call your function below:
